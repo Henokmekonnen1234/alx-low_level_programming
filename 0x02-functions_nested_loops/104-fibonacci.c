2d57faa8687a3f7cae7c5b564d4e9a4d440cb01a
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-07 05:28:55 -0700

    task 13 fibonacci 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,6 +12,8 @@ void fibonacci(void);
 int main(void)
 {
 	signed long int a = 1, num = 1, sum = 1, b = 1;
+	long a_head, a_tail, b_head, b_tail, sum_head, sum_tail;
+
 	while (a <= 98)
 	{
 		printf("%ld, ",sum);
@@ -19,6 +21,25 @@ int main(void)
 		b = num;
 		num = sum;
 		a++;
+	}
+
+	a_head = b / 1000000000;
+	a_tail = b % 1000000000;
+	b_head = a / 1000000000;
+	b_tail = a % 1000000000;
+
+	for (; counter < 99; counter++)
+	{
+		overflow = (a_tail + b_tail) / 1000000000;
+		sum_tail = (a_tail + b_tail) - (1000000000 * overflow);
+		sum_head = (a_head + b_head) + overflow;
+
+		printf(", %lu%lu", sum_head, sum_tail);
+
+		a_head = b_head;
+		a_tail = b_tail;
+		b_head = sum_head;
+		b_tail = sum_tail;
 	}
 	printf("\n");
 	return (0);
