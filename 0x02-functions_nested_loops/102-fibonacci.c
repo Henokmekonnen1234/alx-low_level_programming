4b1662f80737df3f5689c79d4e9a77aef3e4e6bc
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-07 14:51:32 -0700

    task 14 fibonacci 50)

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main -  entry point
+ *count,n,b,sum: variables
+ *
+ *Descritpion: prints out fibonacci 50
+ *Return: 0
+ */
+int main(void)
+{
+	int count = 0;
+	long a = 1, b = 1, sum = 1;
+
+	while (count <= 50)
+	{
+		printf("%d, ", sum);
+		sum = a + b;
+		a = b;
+		b = sum;
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
