1c107c880e9e4c2375442e806fef3381750b36fd
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-07 15:54:15 -0700

    task 12 multiplicaton table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,6 @@
 #include "main.h"
 
 /**
- * main - check the code
- *n: int type
- * Return: Always 0
- */
-int main(void)
-{
-	print_times_table(3);
-	_putchar('\n');
-	print_times_table(5);
-	_putchar('\n');
-	print_times_table(98);
-	_putchar('\n');
-	print_times_table(12);
-	return (0);
-}
-
-/**
  * print_times_table - prints table
  *@n: int type accept a value
  *
