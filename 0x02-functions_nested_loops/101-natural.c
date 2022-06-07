6daf8aaf872b3112bbbce0c38cfdef19acfea555
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-07 15:05:08 -0700

    task 13 print sum of multiple of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *sum,count: int type
+ *
+ *Description: prints the sum multiple of
+ * 3 and 5 below 1024
+ * Return: 0
+ */
+int main(void)
+{
+	int sum = 0, count = 0;
+
+	while (count < 1024)
+	{
+		if (count % 3 == 0 || count % 5 == 0)
+			sum += count;
+		count++
+	}
+	printf("%d\n", sum);
+	return (0);
+}
