848449b1ec445bb365fda2f71d2494b27d19fdfc
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-07 14:16:50 -0700

    16 fibonacci of 98

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,26 +11,26 @@
 
 int main(void)
 {
-	unsigned long  a = 1, num = 1, sum = 1, b = 1, long_num;
+	int count, long_num;
+	unsigned long a = 1, b = 1, sum = 0;
 	long a_1, a_2, b_1, b_2, sum_1, sum_2;
-	
-	
-	while (a < 92)
+
+	printf("1");
+
+	for (count = 2; count < 93; count++) 
 	{
-		printf("%lu, ",sum);
-		sum = num + b;
-		num = b;
+		sum = a + b;
+		a = b;
 		b = sum;
-		a++;
+		printf(", %lu", sum);
 	}
-	
-	
-	a_1 = num / 1000000000;
-	a_2 = num % 1000000000;
+
+	a_1 = a / 1000000000;
+	a_2 = a % 1000000000;
 	b_1 = b / 1000000000;
 	b_2 = b % 1000000000;
 
-	while(a < 98)
+	for (; count < 99; count++)
 	{
 		long_num = (a_2 + b_2) / 1000000000;
 		sum_2 = (a_2 + b_2) - (1000000000 * long_num);
@@ -42,8 +42,8 @@ int main(void)
 		a_2 = b_2;
 		b_1 = sum_1;
 		b_2 = sum_2;
-		a++;
 	}
+
 	printf("\n");
 	return (0);
 }
