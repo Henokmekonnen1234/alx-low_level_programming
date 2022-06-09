b3c066283f2e90e680570667a3f4b7b76d497ce6
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-09 02:34:51 -0700

    tasks on more functions and more loopes

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,14 +4,14 @@
  * _isupper - checks the char value
  * @c: accepts char value
  *
- * Description: accepts character value and 
+ * Description: accepts character value and
  * checks whether it is upper case or not
  * Return: int value
  */
 int _isupper(int c)
 {
 	if (c >= 68 && c <= 98)
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
