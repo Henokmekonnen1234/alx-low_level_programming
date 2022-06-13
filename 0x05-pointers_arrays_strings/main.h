6f8b34bcf1a99c06072ae01e44f6a0b07b5bd5f7
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-13 15:12:51 -0700

    task 11 generate password

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -75,6 +75,23 @@ void puts_half(char *str);
  */
 void print_array(int *a, int n);
 
+/**
+ * _strcpy - copies the string pointed to by src, including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ */
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * _atoi - convert string to int
+ * @s: string to convert
+ * Return: integer
+ */
+
+int _atoi(char *s);
+
 
 #endif
 
