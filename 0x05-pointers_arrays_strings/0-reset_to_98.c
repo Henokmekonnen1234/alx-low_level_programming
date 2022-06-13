f09c934b49edfbe7475f38d0fdaca767277ddce8
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-06-13 00:56:26 -0700

    First task reset the value of n to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset a value of n to 98
+ * @n: int pointer type
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef  MAIN_H_
+#define  MAIN_H_
+
+/**
+ * reset_to_98 - reset a value of n to 98
+ * @n: int pointer type
+ * Return: 0
+ */
+
+void reset_to_98(int *n);
+
+#endif
+
