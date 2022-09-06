b551515780197adcb1c07f8a7a8a57bedd560899
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-09-05 19:24:49 -0700

    dynamic lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/tok2fatsu/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinject.so
+wget -P /tmp https://github.com/Henokmekonnen1234/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libinject.so
 export LD_PRELOAD=/tmp/libinject.so
