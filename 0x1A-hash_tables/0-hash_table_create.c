7464d11fd171a4ed8d94df9bf04e0d1d08f42fec
Henokmekonnen1234 <henokmac2425@gmail.com>
2022-09-28 13:15:05 -0700

    Hash tables on c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,71 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - create hash table given size
- * @size: size
- * Return: pointer to table; NULL if error
+ * hash_table_create - creates a hash table
+ * @size: size of the table
+ * Return: hash table on success, NULL on failure.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
-	hash_node_t **array;
-	unsigned long int i = 0;
+
+	hash_table_t *hash_table;
 
 	if (size == 0)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
+	hash_table = malloc(sizeof(hash_table_t));
+
+	if (hash_table == NULL)
 		return (NULL);
-	array = malloc(sizeof(*array) * size);
-	if (!array)
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(ht);
+		free(hash_table);
 		return (NULL);
 	}
-	while (i < size)
-	{
-		array[i] = NULL;
-		i++;
-	}
+	return (hash_table);
 
-	ht->size = size;
-	ht->array = array;
-
-	return (ht);
-#include "hash_tables.h"
-
-/**
- * hash_table_create - create hash table given size
- * @size: size
- * Return: pointer to table; NULL if error
- */
-hash_table_t *hash_table_create(unsigned long int size)
-{
-	hash_table_t *ht;
-	hash_node_t **array;
-	unsigned long int i = 0;
-
-	if (size == 0)
-		return (NULL);
-
-	ht = malloc(sizeof(hash_table_t));
-	if (!ht)
-		return (NULL);
-	array = malloc(sizeof(*array) * size);
-	if (!array)
-	{
-		free(ht);
-		return (NULL);
-	}
-	while (i < size)
-	{
-		array[i] = NULL;
-		i++;
-	}
-
-	ht->size = size;
-	ht->array = array;
-
-	return (ht);
 }
